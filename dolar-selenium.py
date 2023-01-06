fbd99d83b6ded72b81619830ae5eac7fe5341426
AtaDemirkiran <atademirkiran@gmail.com>
2023-01-06 22:58:27 +0300

    new-dolar-selenium-

--- dolar-selenium.py
+++ dolar-selenium.py
@@ -2,3 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
+
+
+browser=webdriver.Chrome('C:/Users/ATA/chromedriver_win32/chromedriver.exe')
+
+browser.get('https://www.google.com/')
+time.sleep(3)
